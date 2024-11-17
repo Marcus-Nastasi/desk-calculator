78630b01b41f8420378297ca1de6467efab0402f
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-11-17 02:52:27 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.desk.calculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.desk.calculator to javafx.fxml;
+    exports org.desk.calculator;
+}
---
+++ src/main/java/org/desk/calculator/HelloController.java
@@ -0,0 +1,14 @@
+package org.desk.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
