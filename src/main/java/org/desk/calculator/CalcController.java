126aad1bdaeb99472ae1e7293124fcea66cdc907
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-01-14 20:25:31 -0300

    Refactoring spaces.

--- src/main/java/org/desk/calculator/CalcController.java
+++ src/main/java/org/desk/calculator/CalcController.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalcController {
+
     @FXML
     private TextField calculatorInput;
 
@@ -21,8 +22,7 @@ public class CalcController {
 
     @FXML
     protected void enter() {
-        Pattern pattern = Pattern
-            .compile("(-?[0-9]+(?:\\.[0-9]+)?)([+\\-/*])(-?[0-9]+(?:\\.[0-9]+)?)");
+        Pattern pattern = Pattern.compile("(-?[0-9]+(?:\\.[0-9]+)?)([+\\-/*])(-?[0-9]+(?:\\.[0-9]+)?)");
         Matcher matcher = pattern.matcher(calculatorInput.getText());
         if (matcher.matches()) {
             double result = performCalculation(matcher.group(1), matcher.group(2), matcher.group(3));
